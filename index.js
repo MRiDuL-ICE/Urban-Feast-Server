8a95269f69a015f0dbb870907c49f72da5188913
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-01-30 12:14:17 +0600

    order stats api created

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ async function run() {
     });
 
     // use of aggregate pipeline
-    app.get("/order-stats", async (req, res) => {
+    app.get("/order-stats", verifyToken, verifyAdmin, async (req, res) => {
       const result = await paymentCollection
         .aggregate([
           {
@@ -293,6 +293,14 @@ async function run() {
               _id: "$menuItems.category",
               quantity: { $sum: 1 },
               revenue: { $sum: "$menuItems.price" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: "$_id",
+              quantity: "$quantity",
+              revenue: "$revenue",
             },
           },
         ])
